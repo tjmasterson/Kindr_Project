32483437e0da4194452dc38e54fab39663df48ce
b0xw00d <chasenutile@gmail.com>
2015-04-17 02:28:52 -0500

    couple more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:chase) { User.create(username: "chase", email: "chase@chase.com")}
-  let!(:weed_palace) { Dispensary.create(name: "weed_palace", owner: chase)}
-  let!(:purple_haze) { Strain.create(name: "weed_palace")}
-  let!(:palace_haze) { DispensaryStrain.create(dispensary_id: weed_palace.id, strain_id: purple_haze.id)}
-  let!(:palace_haze_vote) {StrainRating.create(dispensary_strain_id: palace_haze.id, user_id: chase.id)}
-
+  let(:chase) { User.new(username: "chase", email: "chase@chase.com") }
 
   it 'can own dispensaries' do
     expect(chase).to have_many :dispensaries
@@ -16,8 +11,16 @@ RSpec.describe User, type: :model do
     expect(chase).to have_many :dispensary_ratings
   end
 
-  it 'can vote on strains specific to dispensaries' do
-    expect(palace_haze_vote).to belong_to :dispensary_strain
+  it 'can rate a strain' do
+    expect(chase).to have_many :strain_ratings
+  end
+
+  it 'can view all the strains they have voted on' do
+    expect(chase).to have_many :dispensary_strains
+  end
+
+  it 'will be notified when hotlist items are back in stock' do
+    expect(chase).to have_many :notifications
   end
 
 end
