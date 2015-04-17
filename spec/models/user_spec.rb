95bd605232a0f87bbb61278eeb67aa7601d601c7
b0xw00d <chasenutile@gmail.com>
2015-04-17 00:55:45 -0500

    Seeds working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
   let!(:purple_haze) { Strain.create(name: "weed_palace", phone_number: 12345678, website: "weed_palace.com", city_id: 1, owner: chase)}
   let!(:palace_haze) { DispensaryStrain.create(dispensary_id: weed_palace.id, strain_id: purple_haze.id)}
 
+
   it 'can own a dispensary' do
     expect(weed_palace.owner).to be_a(User)
   end
@@ -15,7 +16,7 @@ RSpec.describe User, type: :model do
   end
 
   it 'can vote on strains specific to a dispensary' do
-    expect(chase.)
+    expect(chase.strain_ratings.create(dispensary_strain_id: palace_haze.id, ))
   end
 
 end
