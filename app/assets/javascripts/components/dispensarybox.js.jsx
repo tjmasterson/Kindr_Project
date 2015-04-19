a6faaa194bb482a6de231f30d2f4fc736e803ee2
Taylor Masterson <taylor.j.masterson@gmail.com>
2015-04-18 21:52:57 -0500

    trying to figure out million get requests

---
+++ app/assets/javascripts/components/dispensarybox.js.jsx
@@ -0,0 +1,31 @@
+var DispensaryBox = React.createClass({
+  loadDispensariesFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(response) {
+        console.log(response)
+        console.log("yes")
+        this.setState({data: response});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log("no")
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadDispensariesFromServer();
+    setInterval(this.loadDispensariesFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="row">
+      <DispensaryList data={this.state.data} />
+      </div>
+      );
+  }
+});
