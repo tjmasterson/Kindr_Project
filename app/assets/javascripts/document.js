dea60b229e2462683a87354794a1d91db6671cd9
Taylor Masterson <taylor.j.masterson@gmail.com>
2015-04-20 12:16:31 -0500

    ajax not working yet

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -10,38 +10,66 @@ $(document).ready(function(){
 
   var $allDispensaries = $("#all_dispensaries");
 
+  var $navBar = $("#our_nav_bar li a");
+
+  var $homePage = $("#home_page");
+
   var $allStrains = $("#all_strains");
+
+  var $allDispensaries = $("#all_dispensaries");
 
   var addRandomDispensaries = function(dispensary){
     $randomDispensaries.append( dispensaryMarkupGenerator(dispensary) );
-  }
+  };
+
+  var randomDispensariesLoad = function(){
+    $.get('/dispensaries/random_dispensaries').done(function(response){
+      response.forEach(function(dispensaryData){
+        addRandomDispensaries( {name: dispensaryData.name} );
+      });
+    });
+  };
+
+  $navBar.on("click", "a", function(){
+    console.log("rand dis")
+    randomDispensariesLoad();
+  }).bind(this);
 
   var addAllDispensaries = function(dispensary){
     $allDispensaries.append( dispensaryMarkupGenerator(dispensary) );
-  }
+  };
+
+  var allDispensariesLoad = function(){
+    $.get('/dispensaries/dispensaries_index_json').done(function(response){
+      response.forEach(function(dispensaryData){
+        addAllDispensaries( {name: dispensaryData.name} );
+      });
+    });
+  };
+
+  $navBar.on("click", "a", function(){
+    console.log("all dis")
+    allDispensariesLoad();
+  }).bind(this);
+
 
   var addAllStrains = function(strain){
     $allStrains.append( strainMarkupGenerator(strain) );
-  }
+  };
 
-  $.get('/dispensaries/random_dispensaries').done(function(response){
-    response.forEach(function(dispensaryData){
-      addRandomDispensaries( {name: dispensaryData.name} );
+  var allStrainsLoad = function() {
+    $.get('/strains/strains_index_json').done(function(response){
+      response.forEach(function(strainData){
+        addAllStrains( {name: strainData.name} );
+      });
     });
-  });
+  };
 
+  $navBar.on("click", $allStrains, function(){
+    console.log("all str")
+    allStrainsLoad();
+  }).bind(this);
 
-  $.get('/dispensaries/dispensaries_index_json').done(function(response){
-    response.forEach(function(dispensaryData){
-      addAllDispensaries( {name: dispensaryData.name} );
-    });
-  });
-
-  $.get('/strains/strains_index_json').done(function(response){
-    response.forEach(function(strainData){
-      addAllStrains( {name: strainData.name} );
-    });
-  });
 });
 
 
