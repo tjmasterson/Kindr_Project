3ddb7407de226f711a946f837cf24f99be7762dd
Taylor Masterson <taylor.j.masterson@gmail.com>
2015-04-20 10:05:28 -0500

    html changes

--- app/assets/javascripts/components/dispensary.js.jsx
+++ app/assets/javascripts/components/dispensary.js.jsx
@@ -4,7 +4,7 @@ var Dispensary = React.createClass({
     <div className="panel panel-default">
       <div className="col-sm-4 col-xs-6">
         <div className="dispensary_item">
-          <p>{this.props.name}</p><p></p>
+          <p><h4 className="dispensary_item_name">{this.props.name}</h4></p><p></p>
           <div className="panel-thumbnail">
             <a href="#" title="Renovations"><img src="//placehold.it/600x400/444/F8F8F8" className="img-responsive"/></a>
           </div>
