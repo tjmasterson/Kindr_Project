e525f64c58396749616c7e5cf7e64b522e3849f7
Chris Lintecum <clintecum2@gmail.com>
2015-04-21 17:56:21 -0500

    features specs framed but not implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '/login'
+      redirect_to '/'
     else
       render :signup
     end
