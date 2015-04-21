2443fc1d9a91e625f69be73a418c2222cfeebfb9
b0xw00d <chasenutile@gmail.com>
2015-04-21 16:40:45 -0500

    working it out

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,14 +1,49 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    d3.select(".chart")
-        .selectAll("div")
-        .data(data)
-        .enter().append("div")
-        .style("width", function(d) {
-            return d * 10 + "px";
-        })
-        .text(function(d) {
-            return d;
-        });
+// var data = [10,20,30,40,50]
+// var barPadding = 1;
+// var w = 500;
+// var h = 100;
+// var margin = {top: 20, right: 10, bottom: 20, left: 10};
 
-}); //document ready
+// var canvas = d3.select(".chart")
+//             .append("svg")
+//             .attr("width", 200)
+//             .attr("height", 50);
+
+//     d3.select("svg")
+//         .selectAll("rect")
+//         .data(data)
+//         .enter()
+//         .append("rect")
+//         .attr("width", function(d) {
+//             console.log(d);
+//             return d * 5 - barPadding;
+//         })
+//         // .attr("width", w / data.length - barPadding)
+//         .attr("height", 100)
+//         .text(function(d) {
+//             return d;
+//         });
+
+    // $(".chart").load(function() {
+    //     $(".first").transition()
+    //         .attr("x", 320)
+    //         .ease("elastic");
+    // });
+
+// var svg = d3.select("chart")
+//             .append("svg")
+//             .attr("width", w)
+//             .attr("height", h);
+
+// var svg.selectAll("rect")
+//        .data(dataset)
+//        .enter()
+//        .append("rect")
+//        .attr("x", 0)
+//        .attr("y", 0)
+//        .attr("width", 20)
+//        .attr("height", 100);
+
+// }); //document ready
